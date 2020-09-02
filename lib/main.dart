ce86e6c24cc740da88c3f2246df4ba8fb93adc33
GeovanneAtanazio <geovanne7523@gmail.com>
2020-09-01 23:00:49 -0300

    Preparando a aplicação para receber dados futuros

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const request =
     "https://api.hgbrasil.com/finance?format=json-cors&key=ab2d25a5";
 
 void main() async {
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)['results']['currencies']['USD']);
-
+  print(await getData());
   runApp(MaterialApp(
-    home: Container()
+    home: Home()
   ));
+}
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('\$ Conversor de Moedas \$'),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+
+        actions: <Widget>[
+        ],
+      ),
+
+    );
+  }
 }
 
